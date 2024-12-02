981dd8f5d7e2e03fae17364319c1ecbc94dc158c
minagishl <mvxd568.co@icloud.com>
2024-12-02 19:14:40 +0900

    Unify code styles and simplify redundant codes

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,13 @@
-const { build } = require('esbuild');
-const glob = require('glob');
-const fs = require('fs');
-const archiver = require('archiver');
-const entryPoints = glob.sync('./src/**/*.ts');
+const { build } = require("esbuild");
+const glob = require("glob");
+const fs = require("node:fs");
+const archiver = require("archiver");
+const entryPoints = glob.sync("./src/**/*.ts");
 
 const config = {
   entryPoints,
-  outdir: './dist',
-  platform: 'node',
+  outdir: "./dist",
+  platform: "node",
   minify: true,
   bundle: true,
 };
@@ -17,62 +17,64 @@ const config = {
     await build({ ...config });
 
     // images folder copy
-    fs.mkdirSync('./dist/images', { recursive: true });
-    const images = fs.readdirSync('./images');
-    images.forEach((image) => {
+    fs.mkdirSync("./dist/images", { recursive: true });
+    const images = fs.readdirSync("./images");
+    for (const image of images) {
       fs.copyFileSync(`./images/${image}`, `./dist/images/${image}`);
-    });
+    }
 
-    fs.copyFileSync('./manifest.json', './dist/manifest.json');
+    fs.copyFileSync("./manifest.json", "./dist/manifest.json");
 
-    fs.mkdirSync('./dist/firefox', { recursive: true });
+    fs.mkdirSync("./dist/firefox", { recursive: true });
 
     // Copy manifest.json to firefox directory
-    fs.copyFileSync('./manifest.json', './dist/firefox/manifest.json');
+    fs.copyFileSync("./manifest.json", "./dist/firefox/manifest.json");
 
     // Modify the copied manifest.json
-    const data = fs.readFileSync('./dist/firefox/manifest.json', 'utf8');
+    const data = fs.readFileSync("./dist/firefox/manifest.json", "utf8");
     const manifest = JSON.parse(data);
     manifest.browser_specific_settings = {
       gecko: {
-        id: 'minagishl@icloud.com',
+        id: "minagishl@icloud.com",
       },
     };
     manifest.background = {
-      scripts: ['background.js'],
+      scripts: ["background.js"],
     };
-    delete manifest.content_scripts[0].run_at; // Remove the run_at key
+    // Remove the run_at key
+    const { run_at, ...contentScript } = manifest.content_scripts[0];
+    manifest.content_scripts[0] = contentScript;
     fs.writeFileSync(
-      './dist/firefox/manifest.json',
+      "./dist/firefox/manifest.json",
       JSON.stringify(manifest, null, 2),
-      'utf8',
+      "utf8",
     );
 
     // Copy all files except manifest.json to firefox
-    const files = fs.readdirSync('./dist');
-    files.forEach((file) => {
-      if (file !== 'firefox' && file !== 'manifest.json' && file !== 'images') {
+    const files = fs.readdirSync("./dist");
+    for (const file of files) {
+      if (file !== "firefox" && file !== "manifest.json" && file !== "images") {
         fs.copyFileSync(`./dist/${file}`, `./dist/firefox/${file}`);
       }
-    });
+    }
 
     // images folder copy
-    fs.mkdirSync('./dist/firefox/images', { recursive: true });
-    const firefoxImages = fs.readdirSync('./images');
-    firefoxImages.forEach((image) => {
+    fs.mkdirSync("./dist/firefox/images", { recursive: true });
+    const firefoxImages = fs.readdirSync("./images");
+    for (const image of firefoxImages) {
       fs.copyFileSync(`./images/${image}`, `./dist/firefox/images/${image}`);
-    });
+    }
 
     // Create a zip file
-    const output = fs.createWriteStream('./dist/firefox.zip');
-    const archive = archiver('zip', {
+    const output = fs.createWriteStream("./dist/firefox.zip");
+    const archive = archiver("zip", {
       zlib: { level: 9 }, // Sets the compression level.
     });
 
-    output.on('close', function () {});
+    output.on("close", () => {});
 
-    archive.on('warning', function (err) {
-      if (err.code === 'ENOENT') {
+    archive.on("warning", (err) => {
+      if (err.code === "ENOENT") {
         // log warning
       } else {
         // throw error
@@ -80,23 +82,23 @@ const config = {
       }
     });
 
-    archive.on('error', function (err) {
+    archive.on("error", (err) => {
       throw err;
     });
 
     archive.pipe(output);
 
     // append files from a directory
-    const firefoxFiles = fs.readdirSync('./dist/firefox');
-    firefoxFiles.forEach((file) => {
+    const firefoxFiles = fs.readdirSync("./dist/firefox");
+    for (const file of firefoxFiles) {
       archive.file(`./dist/firefox/${file}`, { name: file });
-    });
+    }
 
     // finalize the archive (ie we are done appending files but streams have to finish yet)
     archive
       .finalize()
       .then(() => {
-        fs.rm('./dist/firefox', { recursive: true }, (err) => {
+        fs.rm("./dist/firefox", { recursive: true }, (err) => {
           if (err) throw err;
         });
       })
